84af4b3589cfc80110353c74e7ec2a7c8593e6b4
Traqueur_ <traqueur.dev@gmail.com>
2026-03-26 08:59:42 +0100

    chore: bump version to 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "fr.traqueur.conduit"
-    version = "1.0.0"
+    version = "1.1.0"
 
     extra.set("classifier", System.getProperty("archive.classifier"))
     extra.set("sha", System.getProperty("github.sha"))
